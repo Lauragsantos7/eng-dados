86089973d4ec2fa0ac4bd6f99ae10ac130afebc1
lauragsantos7 <lauragsantos7@gmail.com>
2023-11-07 11:22:52 -0300

    algoritmos

---
+++ Algoritmos/Resumo.md
@@ -0,0 +1,19 @@
+# Algoritmos
+
+Sequência de passos.\
+Definição clara da entrada e do que é esperado na saída.\
+Início -> entrada -> Processamentos -> Saída -> Final
+
+**Tipos de dados**
+- number
+- float
+- string
+- boolean
+
+**Estruturas de decisão**\
+if else, switch case\
+**Estruturas de repetição**\
+for, while, while do
+
+**Funções**\
+Resposnsabilidades únicas, reaproveitar código.
---
+++ Algoritmos/hello.py
@@ -0,0 +1,27 @@
+def hello():
+    name = input("Digite seu nome: ")
+    print(f"Bom dia, {name}")
+
+
+
+def example_cash_machine():
+    balance = 1000
+    value = int(input("""    Digite
+          1 para saque
+          2 para saldo
+          3 para extrato: """))
+    if value == 1:
+        withdraw = int(input("Digite o valor que deseja sacar: "))
+        if withdraw > balance:
+            print('Valor de saldo insuficiente')
+        else: print('Saque realizado com com sucesso!')
+    if value == 2:
+        print(f"Seu saldo é R$ {balance}")
+    if value == 3:
+        print("Carregando extrato atualizado. Aguarde..")
+    if value > 3 or value < 1:
+        print("Número inválido. Digite uma opção válida")
+        (example_cash_machine())
+    
+
+example_cash_machine()
